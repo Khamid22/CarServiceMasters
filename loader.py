9857b23070b9b44da1429e179ea7d6110470eff9
tilonuz <59096186+tilonuz99@users.noreply.github.com>
2021-11-21 20:41:43 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -7,5 +7,6 @@ bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 bot2 = Bot(token=config.BOT2_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+dp2 = Dispatcher(bot2, storage=storage)
 
 Database: MySQLStorage = MySQLStorage("car_service", user='turin', password='qwerty12')
